224f05e89ea0c1dcf13b5ef68ed429b3c0728c4e
Rithik Balasubramanian <rithikrithik222@gmail.com>
2026-01-09 12:41:52 +0530

    Updates

--- _next/static/kMsg-FBR9mJYT7a4y-sL5/_buildManifest.js
+++ _next/static/kMsg-FBR9mJYT7a4y-sL5/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/rithik-portfolio//_next/:path+",
-        "destination": "/rithik-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
